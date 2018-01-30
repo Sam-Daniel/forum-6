0440e518cbafb55c2ec9b0f682b5f852b4be63ce
ddany <duminydaniel@gmail.com>
2018-01-31 02:45:00 +0530

    Update login.php

--- login.php
+++ login.php
@@ -13,8 +13,8 @@
 
             <form action="login_action.php" method="POST">
 
-                <input class="input_field" type="text" name="check" placeholder="Enter Username"><br>
-                <input class="input_field" type="password" name="check" placeholder="Enter Password"><br>
+                <input class="input_field" type="text" name="username" placeholder="Enter Username"><br>
+                <input class="input_field" type="password" name="password" placeholder="Enter Password"><br>
                 <input  class="input_field" type="submit" name="submit" value="Log In">
 
             </form>
